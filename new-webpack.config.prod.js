04fbe1acc210bf77aac3b963c85d36b4b1b69bae
Michael Iglesias <miglesias@tierion.com>
2019-01-16 16:56:00 -0800

    * Removed unused webpack config code * Removed extract-text-webpack-plugin (now using mini text extract) * Removed any polyfill npm dependencies as we are now using @babel/polyfill * Removed other build related packages no longer being used.

--- new-webpack.config.prod.js
+++ new-webpack.config.prod.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const autoprefixer = require('autoprefixer')
 const prefixer = require('postcss-prefixer')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const paths = require('./config/paths')
@@ -19,7 +18,6 @@ module.exports = {
   entry: ['@babel/polyfill', path.resolve('./src/index.js')],
   output: {
     filename: 'bundle.js',
-    // filename: `[name].js`,
     chunkFilename: '[name].chunk.js',
     path: path.resolve(__dirname, 'dist')
   },
@@ -45,18 +43,6 @@ module.exports = {
           }
         ]
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ExtractTextPlugin.extract({
-      //     fallback: "style-loader",
-      //     use: [
-      //       'style-loader',
-      //       'css-loader'
-      //     ]
-      //   }),
-        
-      //   exclude: /\.module\.css$/
-      // },
       {
         test: /\.(sa|sc|c|le)ss$/,
         use: [
@@ -90,107 +76,6 @@ module.exports = {
           'less-loader'
         ],
       },
-      // {
-      //   test: /\.css$/,
-      //   loader: ExtractTextPlugin.extract(
-      //     Object.assign(
-      //       {
-      //         fallback: {
-      //           loader: require.resolve('style-loader'),
-      //           options: {
-      //             hmr: false
-      //           }
-      //         },
-      //         use: [
-      //           {
-      //             loader: require.resolve('css-loader'),
-      //           },
-      //           {
-      //             loader: require.resolve('postcss-loader'),
-      //             options: {
-      //               // Necessary for external CSS imports to work
-      //               // https://github.com/facebookincubator/create-react-app/issues/2677
-      //               ident: 'postcss',
-      //               plugins: () => [
-      //                 prefixer({
-      //                   prefix: 'CCW--',
-      //                   ignore: [/#/, 'isvg', 'loaded']
-      //                 }),
-      //                 require('postcss-flexbugs-fixes'),
-      //                 autoprefixer({
-      //                   browsers: [
-      //                     '>1%',
-      //                     'last 4 versions',
-      //                     'Firefox ESR',
-      //                     'not ie < 9' // React doesn't support IE8 anyway
-      //                   ],
-      //                   flexbox: 'no-2009'
-      //                 })
-      //               ]
-      //             }
-      //           }
-      //         ]
-      //       }
-      //     )
-      //   )
-      //   // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
-      // },
-      // {
-      //   test: /\.less$/,
-      //   loader: ExtractTextPlugin.extract(
-      //     Object.assign(
-      //       {
-      //         fallback: {
-      //           loader: require.resolve('style-loader'),
-      //           options: {
-      //             hmr: false
-      //           }
-      //         },
-      //         use: [
-      //           {
-      //             loader: require.resolve('css-loader')
-      //           },
-      //           {
-      //             loader: require.resolve('postcss-loader'),
-      //             options: {
-      //               // Necessary for external CSS imports to work
-      //               // https://github.com/facebookincubator/create-react-app/issues/2677
-      //               ident: 'postcss',
-      //               plugins: () => [
-      //                 prefixer({
-      //                   prefix: 'CCW--',
-      //                   ignore: [/#/, 'isvg', 'loaded']
-      //                 }),
-      //                 require('postcss-flexbugs-fixes'),
-      //                 autoprefixer({
-      //                   browsers: [
-      //                     '>1%',
-      //                     'last 4 versions',
-      //                     'Firefox ESR',
-      //                     'not ie < 9' // React doesn't support IE8 anyway
-      //                   ],
-      //                   flexbox: 'no-2009'
-      //                 })
-      //               ]
-      //             }
-      //           },
-      //           {
-      //             loader: 'less-loader'
-      //           }
-      //         ]
-      //       }
-      //     )
-      //   )
-      //   // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
-      // },
-      // {
-      //   test: /\.less$/,
-      //   use: [
-      //     'style-loader',
-      //     'css-loader',
-      //     'less-loader'
-      //   ]
-      // },
       {
         test: /\.(jpe?g|png|svg|ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
         use: 'base64-inline-loader'
@@ -198,9 +83,6 @@ module.exports = {
     ]
   },
   plugins: [
-    // new ExtractTextPlugin({
-    //   filename: "[name].css"
-    // }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
